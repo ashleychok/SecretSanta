e09ac6fb5ae9ad7a2c2357309148dd5254bcde31
aslikyan <arthur@tidepoollabs.com>
2021-12-22 03:14:01 +0400

    initial commit

---
+++ checker.js
@@ -0,0 +1,3 @@
+if (!localStorage.getItem('loggedIn')) {
+    document.location.href = 'index.html'
+}
---
+++ index.js
@@ -0,0 +1,69 @@
+document.addEventListener('DOMContentLoaded', function(){
+  let word = [];
+  const checkWord = (wordArray) => {
+    const wordFromArray = wordArray.join('');
+    if (wordArray.length === 4 && !wordArray.includes(undefined) && wordFromArray.toLowerCase() !== 'sher') {
+      firstLetter.value = '';
+      secondLetter.value = '';
+      thirdLetter.value = '';
+      fourthLetter.value = '';
+      firstLetter.focus();
+      word = [];
+    }
+    if (wordFromArray.toLowerCase() === 'sher') {
+      localStorage.setItem('loggedIn', 'true')
+      setTimeout(() => document.location.href = 'welcome.html', 500);
+    }
+    console.log(wordFromArray);
+  }
+  const firstLetter = document.getElementById('firstLetter');
+  firstLetter.focus()
+  const secondLetter = document.getElementById('secondLetter');
+  const thirdLetter = document.getElementById('thirdLetter');
+  const fourthLetter = document.getElementById('fourthLetter');
+  console.log(firstLetter)
+  firstLetter.addEventListener('input', (e) => {
+    console.log(123);
+    console.log(e)
+    if (e.target.value) {
+      word[0] = e.target.value;
+      secondLetter.focus();
+      checkWord(word);
+    } else {
+      word.splice(0, 1);
+      checkWord(word);
+    }
+  });
+  secondLetter.addEventListener('input', (e) => {
+    if (e.target.value) {
+      word[1] = e.target.value;
+      thirdLetter.focus();
+      checkWord(word);
+    } else {
+      firstLetter.focus();
+      word.splice(1, 1);
+      checkWord(word);
+    }
+  });
+  thirdLetter.addEventListener('input', (e) => {
+    if (e.target.value) {
+      word[2] = e.target.value;
+      fourthLetter.focus();
+      checkWord(word);
+    } else {
+      secondLetter.focus();
+      word.splice(2, 1);
+      checkWord(word);
+    }
+  });
+  fourthLetter.addEventListener('input', (e) => {
+    if (e.target.value) {
+      word[3] = e.target.value;
+      checkWord(word);
+    } else {
+      thirdLetter.focus();
+      word.splice(3, 1);
+      checkWord(word);
+    }
+  });
+});
