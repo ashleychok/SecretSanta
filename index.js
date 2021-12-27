13f0d6e53fa5e92f9bf48117b510e53b1e928d7d
ashleychok <aslikyanashkhen@gmail.com>
2021-12-27 04:45:59 +0400

    some security changes

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 document.addEventListener('DOMContentLoaded', function(){
+  const secret = '28c5f19f166ad68f350f656104280a744305edac23b5bcbd2d975f2d12721964';
+  const digest = async ({ algorithm = "SHA-256", message }) =>
+      Array.prototype.map
+          .call(
+              new Uint8Array(
+                  await crypto.subtle.digest(algorithm, new TextEncoder().encode(message))
+              ),
+              (x) => ("0" + x.toString(16)).slice(-2)
+          )
+          .join("");
   let word = [];
   const checkWord = (wordArray) => {
     const wordFromArray = wordArray.join('');
-    if (wordArray.length === 4 && !wordArray.includes(undefined) && wordFromArray.toLowerCase() !== 'sher') {
-      firstLetter.value = '';
-      secondLetter.value = '';
-      thirdLetter.value = '';
-      fourthLetter.value = '';
-      firstLetter.focus();
-      word = [];
-    }
-    if (wordFromArray.toLowerCase() === 'sher') {
-      localStorage.setItem('loggedIn', 'true')
-      setTimeout(() => document.location.href = 'welcome.html', 500);
-    }
+    digest({message: wordFromArray.toLowerCase()}).then(data => {
+      if (wordArray.length === 4 && !wordArray.includes(undefined) && data !== secret) {
+        firstLetter.value = '';
+        secondLetter.value = '';
+        thirdLetter.value = '';
+        fourthLetter.value = '';
+        firstLetter.focus();
+        word = [];
+      }
+      if (data === secret) {
+        wdfr();
+      }
+    })
   }
   const firstLetter = document.getElementById('firstLetter');
   firstLetter.focus()
@@ -52,6 +63,9 @@ document.addEventListener('DOMContentLoaded', function(){
       checkWord(word);
     }
   });
+  const rew = () => {
+    setTimeout(() => document.location.href = 'welcome.html', 500);
+  }
   fourthLetter.addEventListener('input', (e) => {
     if (e.target.value) {
       word[3] = e.target.value;
@@ -62,4 +76,8 @@ document.addEventListener('DOMContentLoaded', function(){
       checkWord(word);
     }
   });
+  const wdfr = () => {
+    localStorage.setItem('loggedIn', 'true');
+    rew();
+  }
 });
